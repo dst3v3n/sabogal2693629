e421d73fcbac9883a48865bc04fe85a4f4afc988
dst3v3n <hablack10@gmail.com>
2023-06-06 18:25:24 -0500

    Objetos, Pruebas and Readme.md

--- pruebas/objetos/EmpleadoAPP.py
+++ pruebas/objetos/EmpleadoAPP.py
@@ -10,14 +10,14 @@ persona1.getNombre ("Steven")
 persona1.getCargo ("Multimedia")
 persona1.getSalario (1160000)
 
-print(persona1.setNombre)
-print(persona1.setCargo)
-print(persona1.setSalario)
+print(f"Tu nombre es: {persona1.setNombre}")
+print(f"Tu cargo es: {persona1.setCargo}")
+print(f"Tu salario es: {persona1.setSalario}")
 
-print(persona1.Salario_Hora())
-print(persona1.incrementoSalario())
-print(persona1.hora_extras(2))
-print(persona1.sueldo_total())
+print(f"Tu salario por hora es: {persona1.Salario_Hora()}")
+print(f"Tu incremento salarial es de: {persona1.incrementoSalario()}")
+print(f"Tu incremento salarial por las hora extras es de :{persona1.hora_extras(1)}")
+print(f"Tu sueldo es de: {persona1.sueldo_total()}")
 
 
 
@@ -28,15 +28,14 @@ persona2.getNombre ("Sabogal")
 persona2.getCargo ("Sistemas")
 persona2.getSalario (2460000)
 
-print(persona2.setNombre)
-print(persona2.setCargo)
-print(persona2.setSalario)
+print(f"Tu nombre es: {persona2.setNombre}")
+print(f"Tu cargo es: {persona2.setCargo}")
+print(f"Tu salario es: {persona2.setSalario}")
 
-print(persona2.Salario_Hora())
-print(persona2.incrementoSalario())
-print(persona2.hora_extras(1))
-print(persona2.sueldo_total())
-
+print(f"Tu salario por hora es: {persona2.Salario_Hora()}")
+print(f"Tu incremento salarial es de: {persona2.incrementoSalario()}")
+print(f"Tu incremento salarial por las hora extras es de :{persona2.hora_extras(2)}")
+print(f"Tu sueldo es de: {persona2.sueldo_total()}")
 
 
 
@@ -46,12 +45,12 @@ persona3.getNombre ("Sabogal")
 persona3.getCargo ("Sistemas")
 persona3.getSalario (5500000)
 
-print(persona3.setNombre)
-print(persona3.setCargo)
-print(persona3.setSalario)
+print(f"Tu nombre es: {persona3.setNombre}")
+print(f"Tu cargo es: {persona3.setCargo}")
+print(f"Tu salario es: {persona3.setSalario}")
 
-print(persona3.Salario_Hora())
-print(persona3.incrementoSalario())
-print(persona3.sueldo_total())
+print(f"Tu salario por hora es: {persona3.Salario_Hora()}")
+print(f"Tu incremento salarial es de: {persona3.incrementoSalario()}")
+print(f"Tu sueldo es de: {persona3.sueldo_total()}")
 
 print(persona3.counter)
