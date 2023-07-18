bbcaaab04268da83d3b5aabd5151409a87bbb2d3
dst3v3n <hablack10@gmail.com>
2023-07-18 13:19:05 -0500

    Exception, raise and assert

--- excepciones/ejercicioexp1.py
+++ excepciones/ejercicioexp1.py
@@ -1,9 +1,47 @@
-def Cuadratica ():
+### Assert ###
+
+def Cuadratica_assert ():
     try:
         a = float(input("Digita un numero para el valor A: "))
         b = float(input("Digita un numero para el valor B: "))
         c = float(input("Digita un numero para el valor C: "))
-        x = b**2- (4*a*c)
+
+        x = b**2 - (4*a*c)
+        
+        assert x>0
+        
+        raiz = x**0.5
+
+        sustraccion = -b -raiz
+        adicion = -b + raiz
+
+        assert not(a == 0)
+        
+        divi_sus = sustraccion/(2*a)
+        divi_adi = adicion/(2*a)
+
+
+        print(f"La ecuacion cuadratica de la suma es: {divi_adi}")
+        print(f"La ecuacion cuadratica de la resta es: {divi_sus}")
+
+    except AssertionError:
+        print("ohh... ah ocurrido un error!. Buscalo!")
+    except ValueError:
+        print("No se puede ingresar tipos de datos que no sean enteros o flotantes")
+    except KeyboardInterrupt:
+        print("Se ha interrumpido el proceso del programa")
+
+Cuadratica_assert ()
+
+### Raise ####
+
+def Cuadratica_raise ():
+    try:
+        a = float(input("Digita un numero para el valor A: "))
+        b = float(input("Digita un numero para el valor B: "))
+        c = float(input("Digita un numero para el valor C: "))
+        
+        x = b**2 - (4*a*c)
         
         if not(x>0):
             raise TypeError
@@ -12,12 +50,13 @@ def Cuadratica ():
 
         sustraccion = -b -raiz
         adicion = -b + raiz
-        
+
         if a==0:
             raise ZeroDivisionError
         
-        divi_sus = sustraccion/2*a
-        divi_adi = adicion/2*a
+        divi_sus = sustraccion/(2*a)
+        divi_adi = adicion/(2*a)
+
 
         print(f"La ecuacion cuadratica de la suma es: {divi_adi}")
         print(f"La ecuacion cuadratica de la resta es: {divi_sus}")
@@ -28,7 +67,9 @@ def Cuadratica ():
         print("No se puede ingresar tipos de datos que no sean enteros o flotantes")
     except TypeError:
         print("No se puede sacar raiz de numero negativos")
+    except KeyboardInterrupt:
+        print("Se ha interrumpido el proceso del programa")
     except:
         print("ohh... ah ocurrido un error!. Buscalo!")
 
-Cuadratica ()
+Cuadratica_raise ()
